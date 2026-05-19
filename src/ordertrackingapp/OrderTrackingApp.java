3e82f5c345c725fb15e8c57367c646622d65a2f8
Theodore <sanadabed3333@gmail.com>
2026-05-19 23:11:20 +0300

    src

--- src/ordertrackingapp/OrderTrackingApp.java
+++ src/ordertrackingapp/OrderTrackingApp.java
@@ -10,11 +10,8 @@ package ordertrackingapp;
  */
 public class OrderTrackingApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
